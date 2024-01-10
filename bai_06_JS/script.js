025979008afd051cc2646a81c6af92e63367d0d4
khuonggminhhoang <minhkhuong782k3@gmail.com>
2024-01-10 23:56:17 +0700

    push

--- bai_06_JS/script.js
+++ bai_06_JS/script.js
@@ -143,13 +143,16 @@ function nameStd(name){
     // P8 - Number:
 
     // isNaN(a) : return true nếu a là NaN
+    /*
     var result = '10' / '2';
     console.log(result);
     var res = 10/'a';
     console.log(typeof res == "number");
     console.log(isNaN(res))
+    */
 
     // toString(): chuyển kiểu number thành kiểu string
+    /*
     var a = 10;
     var b = a.toString();
     var c = (10).toString();
@@ -162,13 +165,111 @@ function nameStd(name){
     console.log(typeof c); // Trả về kiểu string
     console.log(d);
     console.log(typeof d);
+    */
 
     // toFixed(digits): làm tròn số và lấy digits số sau dấu phẩy
+    /*
     var num = 12.3456;
     console.log(num.toFixed())
     console.log(num.toFixed(3));
     console.log(num.toPrecision(4)); // lấy 4 số cả sau dấu thập phân
+    */
     
+    // P9 - Array:
+    
+    // toString(): chuyển array thành string, ngăn cách nhau bởi dấu ,
+    var list = ['C++', 'Python', 'HTML', 'CSS', 'JS', 'Java'];
+    console.log(list.toString());
+
+    // join(seperator) : dùng để thêm các seperator vào giữa các phần tử của mảng
+    console.log(list.join(' - '));
+    console.log(list.join(' '));
+    console.log(list.join()); /// giống toString()
+    list.forEach(x => console.log(x));
+
+    // pop(): remove and return the last item of the array
+    var array = ['C++', 'Python', 'HTML', 'CSS', 'JS', 'Java'];
+    console.log(array.toString());
+    console.log(array.pop());
+    console.log(array.pop());
+    console.log(array.pop());
+    console.log(array.pop());
+    console.log(array.pop());
+    console.log(array.pop());
+    console.log(array.pop());   // pop hết thì trả về undefined
+    console.log(array.join(' '));
+
+    // push(item1, item2, item3, ...): thêm phần tử vào cuối mảng, return new length of the array
+    console.log(array.push('JAVA', 'C'));
+    console.log(array.join(' '));
+    var arr = ['JAVA', 'C'];
+    console.log(arr == array);
+    console.log(arr === array);
+
+    // reverse() : đảo ngược array
+    console.log(arr.reverse().join(' '))
+
+    // shift(): xóa và trả về phẩn tử đầu tiên của mảng
+    console.log(arr.shift());
+    console.log(arr);
+
+    // unshift(item1, item2, item3, ...): thêm các item vào đầu array và trả về new length
+    arr.push('HTML', 'CSS', 'PHP', 'PYTHON');
+    console.log(arr);
+    console.log(arr.unshift('JS', 'C++', 'C'));
+    console.log(arr);
+
+    // splice(index, howMany, item1, item2, ...): chèn hoặc xóa howMany phần tử tại vị trí index; return array bị xóa
+    // trong đó index là vị trí thao tác, howMany là số phần tử cần xóa
+    // console.log(arr.splice(2)); // xóa các phần tử từ vị trí index đến cuối
+    console.log(arr.splice(2, 3, 'C#', 'TypeScript'));
+    console.log(arr);
+    // để thêm các phần tử vào vị trí index thì howMany = 0 là okeee :))
+    // để xóa theo ý muốn thì ko cần thêm các item.
+
+    // vidu chèn phàn tử:
+    var list = ["HTML5", "CSS3", "Javascript"];
+    console.log(list.splice(2, 0, "Bootstrap 4", "ReactJS")); // Trả về: []
+    console.log(list);
+    // Trả về: ["HTML5", "CSS3", "Bootstrap 4", "ReactJS", "Javascript"]
+
+    // vidu xóa phần tử:
+    var list = ["HTML5", "CSS3", "Javascript"];
+    console.log(list.splice(1, 1)); // Trả về: ["CSS3"]
+    console.log(list); // Trả về: ["HTML5", "Javascript"]
+
+    // vidu Xóa phần tử và chèn phần tử mới vào mảng
+    var list = ["HTML5", "CSS3", "Javascript"];
+    console.log(list.splice(2, 1, "Bootstrap 4", "ReactJS"));
+    // Trả về: ["Javascript"]
+    console.log(list);
+    // Trả về: ["HTML5", "CSS3", "Bootstrap 4", "ReactJS"]
+    
+    // concat(array2, array3, ..., arrayX) : nối các array với nhau mà ko ảnh hưởng tới array ban đầu
+    var list = ["HTML5", "CSS3", "Javascript"];
+    var list2 = ["Bootstrap 4", "ReactJS"];
+    console.log(list.concat(list2));
+    // Trả về: ["HTML5", "CSS3", "Javascript", "Bootstrap 4", "ReactJS"]
+    console.log(list);
+    // Trả về: ["HTML5", "CSS3", "Javascript"]
+
+    // dùng toán tử spread để nối 2 array:
+    // list.push(...list2);
+    // console.log(list);                       // cách này cũng nối, nhưng làm thay đổi array ban đầu
+
+
+    var resultArray = [...list, ...list2];
+    console.log(resultArray);
+    console.log(...list, ...list2);
+
+
+    // slice(start, end): giống xử lý xâu, cắt từ phần tử index bằng start đến phần tử có index = end - 1
+    console.log(resultArray.slice(3));
+    console.log(resultArray.slice(1, 3));
+    console.log(resultArray.slice(-3, -1));
+    console.log(resultArray);
+    // tương tự python, chỉ khác cú pháp
+
 
 })();
 
